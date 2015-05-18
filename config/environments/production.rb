b8f3579bef059fd65e743fe8e454caafe3647caa
John Costello <costelljohn@gmail.com>
2015-05-18 21:39:01 +0100

    s3 config 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #Refinery::Core.
-  config.s3_backend = true
+  Refinery::Core.config.s3_backend = true
 end
